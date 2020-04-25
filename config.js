c8fb72e4f03dcd76dd888472d262b95930430126
coren <corentin.delage@hotmail.fr>
2020-04-25 18:16:57 +0200

    signin temp + config file

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    host : "localhost",
+    user: "root",  //user mysql
+    password: "",  //password mysql
+    database: "benevent" //db mysql
+}
--- server.js
+++ server.js
@@ -3,13 +3,14 @@ const mysql = require('mysql');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const config = require('./config.js')
 const con = mysql.createConnection({
-    host : "localhost",
-    user: "beneventadmin",  //user mysql
-    password: "admin",  //password mysql
-    database: "benevent" //db mysql
+    host : config.host,
+    user: config.user,  //user mysql
+    password: config.password,  //password mysql
+    database: config.database //db mysql
 });
-
+app.use(express.json());
 con.connect(function(err) {
     if (err) throw err;
     console.log("Connected!");
@@ -19,14 +20,21 @@ app.get('/', function (req, res) {
     res.send('Hello World!')
 });
 
-app.get('/users', function (req, res) {
-        con.query("SELECT * FROM user", function (err, result, fields) {
+app.get('/users', async function (req, res) {
+        await con.query("SELECT * FROM user", function (err, result, fields) {
             if (err) throw err;
             console.log(result);
             res.send(result);
         });
 });
 
-app.listen(3000, function () {
+app.post('/signin', function (req, res) {
+  console.log(req.body);
+    var email = req.body.login;
+    var password = req.body.password;
+    res.status(200).send();
+});
+
+app.listen(PORT, function () {
     console.log('Example app listening on port 3000!')
 });
