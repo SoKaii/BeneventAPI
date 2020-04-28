c0103a4dea5087453186cc85cb1d775f96a253c2
coren <corentin.delage@hotmail.fr>
2020-04-28 16:29:55 +0200

    insert de base

--- bdd.sql
+++ bdd.sql
@@ -11,16 +11,18 @@ CREATE TABLE user(
         idu           Int  Auto_increment  NOT NULL ,
         name          Varchar (50) NOT NULL ,
         firstname     Varchar (50) NOT NULL ,
-        age           Date NOT NULL ,
-        email         Varchar (100) NOT NULL UNIQUE,
-    		password	  Varchar (50) NOT NULL ,
-        phone         Varchar (12) ,
+		age			Date NOT NULL,
+        email         Varchar (100) NOT NULL ,
+		password      Varchar (50) NOT NULL,
+        phone         Text ,
         profilpicture Blob ,
-        address       Text NOT NULL ,
-        description   Text NOT NULL
+        address       Text ,
+        description   Varchar (250)         
 	,CONSTRAINT user_PK PRIMARY KEY (idu)
 )ENGINE=InnoDB;
 
+INSERT INTO user(name,firstname,age,email,password) values("test","test","1998-03-19","test@test.fr","test");
+INSERT INTO user(name,firstname,age,email,password) values("John","Doe","1988-05-28","jd@test.fr","JohnDoe");
 
 #------------------------------------------------------------
 # Table: category
@@ -31,28 +33,32 @@ CREATE TABLE category(
         name  Varchar (255) NOT NULL
 	,CONSTRAINT category_PK PRIMARY KEY (idcat)
 )ENGINE=InnoDB;
-
+INSERT INTO category(name) values("festival");
+INSERT INTO category(name) values("humanitary");
+INSERT INTO category(name) values("homeless");
 
 #------------------------------------------------------------
 # Table: association
 #------------------------------------------------------------
 
 CREATE TABLE association(
-        idas    	Int  Auto_increment  NOT NULL ,
-        name    	Varchar (50) NOT NULL ,
-        logo    	Blob ,
-        acronym 	Varchar (50) ,
-        email       Varchar (100) NOT NULL ,
-		password	Varchar (50) NOT NULL ,
-        phone   	Varchar (50) NOT NULL ,
-        website 	Varchar (50) NOT NULL ,
-        support 	Varchar (50) NOT NULL ,
-        idcat   	Int NOT NULL
+        idas     Int  Auto_increment  NOT NULL ,
+        name     Varchar (50) NOT NULL ,
+        logo     Blob ,
+        acronym  Varchar (50) ,
+        email    Varchar (50) NOT NULL ,
+        phone    Varchar (50) ,
+        website  Varchar (50) ,
+        support  Varchar (50) ,
+        password Varchar (50) NOT NULL ,
+        idcat    Int NOT NULL
 	,CONSTRAINT association_PK PRIMARY KEY (idas)
 
 	,CONSTRAINT association_category_FK FOREIGN KEY (idcat) REFERENCES category(idcat)
 )ENGINE=InnoDB;
 
+INSERT INTO association(name,email,password,idcat) values("test","test@test.fr","test",1);
+INSERT INTO association(name,email,password,idcat) values("Benevolat","bene@test.fr","Benevolat",2);
 
 #------------------------------------------------------------
 # Table: event
@@ -118,7 +124,24 @@ CREATE TABLE type(
         name Varchar (20) NOT NULL
 	,CONSTRAINT type_PK PRIMARY KEY (idty)
 )ENGINE=InnoDB;
+INSERT INTO type(name) values("bug");
+INSERT INTO type(name) values("improvement");
 
+#------------------------------------------------------------
+# Table: admin
+#------------------------------------------------------------
+
+CREATE TABLE admin(
+        ida      Int  Auto_increment  NOT NULL ,
+        login    Varchar (20) NOT NULL ,
+        password Varchar (50) NOT NULL ,
+        email    Varchar (50) NOT NULL
+	,CONSTRAINT admin_PK PRIMARY KEY (ida)
+)ENGINE=InnoDB;
+
+INSERT INTO admin(login,password,email) values("admin_01","corentin","cocun91@gmail.com");
+INSERT INTO admin(login,password,email) values("admin_02","matthieu","habib.matthieu1@gmail.com");
+INSERT INTO admin(login,password,email) values("admin_03","thomas","t.martin92500@hotmail.fr");
 
 #------------------------------------------------------------
 # Table: feedback
@@ -129,16 +152,22 @@ CREATE TABLE feedback(
         title   Varchar (50) NOT NULL ,
         content Text NOT NULL ,
         date    Datetime NOT NULL ,
+        status  Varchar (50) NOT NULL ,
         idas    Int ,
         idty    Int NOT NULL ,
-        idu     Int
+        idu     Int ,
+        ida     Int
 	,CONSTRAINT feedback_PK PRIMARY KEY (idfe)
 
 	,CONSTRAINT feedback_association_FK FOREIGN KEY (idas) REFERENCES association(idas)
 	,CONSTRAINT feedback_type0_FK FOREIGN KEY (idty) REFERENCES type(idty)
 	,CONSTRAINT feedback_user1_FK FOREIGN KEY (idu) REFERENCES user(idu)
+	,CONSTRAINT feedback_admin2_FK FOREIGN KEY (ida) REFERENCES admin(ida)
 )ENGINE=InnoDB;
 
+INSERT INTO feedback(title,content,date,status,idty,idu) values("Launch Screen Bug","black screen during 5s on launching app",sysdate(),"",1,1);
+INSERT INTO feedback(title,content,date,status,idty,idas) values("cannot remove my account","when i remove my account they said ok but i could again connect me",sysdate(),"",1,2);
+INSERT INTO feedback(title,content,date,status,idty,idu) values("improvement of login screen","maybe that gonna be better if login button was bigger",sysdate(),"",2,2);
 
 #------------------------------------------------------------
 # Table: followers
@@ -162,7 +191,9 @@ CREATE TABLE participation(
         idev        Int NOT NULL ,
         idu         Int NOT NULL ,
         participate Bool NOT NULL ,
-        status      Bool NOT NULL
+        status      Bool NOT NULL ,
+        startdate   Datetime NOT NULL ,
+        enddate     Datetime NOT NULL
 	,CONSTRAINT participation_PK PRIMARY KEY (idev,idu)
 
 	,CONSTRAINT participation_event_FK FOREIGN KEY (idev) REFERENCES event(idev)
@@ -182,3 +213,5 @@ CREATE TABLE interest(
 	,CONSTRAINT interest_event_FK FOREIGN KEY (idev) REFERENCES event(idev)
 	,CONSTRAINT interest_user0_FK FOREIGN KEY (idu) REFERENCES user(idu)
 )ENGINE=InnoDB;
+
+
