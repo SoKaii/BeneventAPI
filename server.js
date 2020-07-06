6d2a86f665dfe6f41865422dd3cfece4b78ce74a
coren <corentin.delage@hotmail.fr>
2020-07-06 15:41:47 +0200

    participation route

--- server.js
+++ server.js
@@ -597,7 +597,7 @@ app.post('/participate',function(req, res){
   });
 });
 
-app.patch('/participaterefuse',function(req, res){
+app.patch('/participate/refuse',function(req, res){
   const { idev } = req.body;
   const { idu } = req.body;
   const { status } = req.body;
@@ -618,45 +618,54 @@ app.patch('/participaterefuse',function(req, res){
   }
 });
 
-app.patch('/participatestart',function (req, res){
-  const { idev } = req.body;
-  const { idu } = req.body;
-  const { status } = req.body;
-  if (status == 1) {
-    con.query({
-        sql: 'UPDATE `participation` SET `startdate`=now(),participate=1 WHERE `idev` = ? AND `idu` = ?',
-        values: [idev,idu]
-    }, function (err, result, fields) {
-        if (err) {
-            res.status(500).send({error: "Internal Server Error"});
-        }
-        console.log(result);
-        res.status(200).send();
-    });
-  }else{
-      res.status(400).send();
-  }
-});
-app.patch('/participateend',function (req, res){
-  const { idev } = req.body;
-  const { idu } = req.body;
-  const { status } = req.body;
-  if (status == 1) {
-    con.query({
-        sql: 'UPDATE `participation` SET `enddate`=now() WHERE `idev` = ? AND `idu` = ?',
-        values: [idev,idu]
-    }, function (err, result, fields) {
-        if (err) {
-            res.status(500).send({error: "Internal Server Error"});
-        }
-        console.log(result);
-        res.status(200).send();
-    });
-  }else{
-      res.status(400).send();
-  }
-});
 
+app.patch('/participate/startstop',function (req, res){
+  const { idev } = req.body;
+  const { idu } = req.body;
+  const { status } = req.body;
+  console.log("test");
+
+    con.query({
+        sql: 'SELECT `startdate` FROM `participation` WHERE `idev` = ? and `idu` = ?',
+        values: [idev,idu]
+    }, function (err, result, fields) {
+        if (err) {
+            res.status(500).send({error: "Internal Server Error"});
+        }
+        console.log(result[0].startdate);
+        if (result[0].startdate==null) {
+          if (status == 1) {
+            con.query({
+                sql: 'UPDATE `participation` SET `startdate`=now(),participate=1 WHERE `idev` = ? AND `idu` = ?',
+                values: [idev,idu]
+            }, function (err, result, fields) {
+                if (err) {
+                    res.status(500).send({error: "Internal Server Error"});
+                }
+                console.log(result);
+                res.status(200).send();
+            });
+          }else{
+              res.status(400).send();
+          }
+        }else{
+          if (status == 1) {
+            con.query({
+                sql: 'UPDATE `participation` SET `enddate`=now() WHERE `idev` = ? AND `idu` = ?',
+                values: [idev,idu]
+            }, function (err, result, fields) {
+                if (err) {
+                    res.status(500).send({error: "Internal Server Error"});
+                }
+                console.log(result);
+                res.status(200).send();
+            });
+          }else{
+              res.status(400).send();
+          }
+        }
+    });
+});
 // MEDIA ROUTES
 
 app.get('/medias', function (req, res) {
@@ -710,81 +719,6 @@ app.get('/type/:idty', function (req, res) {
         res.status(200).send(result);
     });
 }); // récupérer le type d'id
-
-app.post('/participate',function(req, res){
-  const { idev } = req.body;
-  const { idu } = req.body;
-  con.query({
-      sql: 'INSERT INTO `participation`(`idev`,`idu`,`participate`,`status`) VALUES(?,?,0,1)',
-      values: [idev,idu]
-  }, function (err, result, fields) {
-      if (err) {
-          res.status(500).send({error: err});
-      }
-      console.log(result);
-      res.status(200).send();
-  });
-});
-
-app.patch('/participaterefuse',function(req, res){
-  const { idev } = req.body;
-  const { idu } = req.body;
-  const { status } = req.body;
-
-  if (status == 1) {
-    con.query({
-        sql: 'UPDATE `participation` SET `status`=0,participate=0 WHERE `idev` = ? AND `idu` = ?',
-        values: [idev,idu]
-    }, function (err, result, fields) {
-        if (err) {
-            res.status(500).send({error: "Internal Server Error"});
-        }
-        console.log(result);
-        res.status(200).send();
-    });
-  }else{
-      res.status(400).send();
-  }
-});
-
-app.patch('/participatestart',function (req, res){
-  const { idev } = req.body;
-  const { idu } = req.body;
-  const { status } = req.body;
-  if (status == 1) {
-    con.query({
-        sql: 'UPDATE `participation` SET `startdate`=now(),participate=1 WHERE `idev` = ? AND `idu` = ?',
-        values: [idev,idu]
-    }, function (err, result, fields) {
-        if (err) {
-            res.status(500).send({error: "Internal Server Error"});
-        }
-        console.log(result);
-        res.status(200).send();
-    });
-  }else{
-      res.status(400).send();
-  }
-});
-app.patch('/participateend',function (req, res){
-  const { idev } = req.body;
-  const { idu } = req.body;
-  const { status } = req.body;
-  if (status == 1) {
-    con.query({
-        sql: 'UPDATE `participation` SET `enddate`=now() WHERE `idev` = ? AND `idu` = ?',
-        values: [idev,idu]
-    }, function (err, result, fields) {
-        if (err) {
-            res.status(500).send({error: "Internal Server Error"});
-        }
-        console.log(result);
-        res.status(200).send();
-    });
-  }else{
-      res.status(400).send();
-  }
-});
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000 !');
